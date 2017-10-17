eeeddb3c09040190513621fa6d44ef103a7b9725
SrGaara <nikel_black@hotmail.com>
2017-10-17 18:59:29 -0500

    function call phone

--- app/src/main/java/com/example/srgaara/phonecontacts/MainActivity.java
+++ app/src/main/java/com/example/srgaara/phonecontacts/MainActivity.java
@@ -5,7 +5,9 @@ import java.util.ArrayList;
 import android.Manifest;
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.net.Uri;
@@ -14,6 +16,7 @@ import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -21,14 +24,16 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 public class MainActivity extends Activity {
+    private static final int MY_PERMISSIONS_REQUEST_CALL_PHONE = 2 ;
     private ListView mListView;
     private static final int yolo =1;
     private ProgressDialog pDialog;
     private Handler updateBarHandler;
     ArrayList<String> contactList;
+    ArrayList<String> phonenomber;
     Cursor cursor;
     int counter;
-    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS =1;
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
     @Override
    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,12 +63,26 @@ public class MainActivity extends Activity {
                 // result of the request.
             }
         }
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CALL_PHONE)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSIONS_REQUEST_CALL_PHONE);
+            }
+        }
+
         pDialog = new ProgressDialog(this);
         pDialog.setMessage("Reading contacts...");
         pDialog.setCancelable(false);
         pDialog.show();
         mListView = (ListView) findViewById(R.id.list);
-        updateBarHandler =new Handler();
+        updateBarHandler = new Handler();
         // Since reading contacts takes more time, let's run it on a separate thread.
         new Thread(new Runnable() {
             @Override
@@ -72,17 +91,48 @@ public class MainActivity extends Activity {
             }
         }).start();
         // Set onclicklistener to the list item.
-        mListView.setOnItemClickListener(new OnItemClickListener() {
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
                 //TODO Do whatever you want with the list data
-                Toast.makeText(getApplicationContext(), "item clicked : \n"+contactList.get(position), Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "item clicked : \n"+ contactList.get(position), Toast.LENGTH_SHORT).show();
+                String phone;
+                call(phonenomber.get(position));
             }
         });
     }
+    private void call(String tel) {
+
+        try {
+
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:"+tel));
+            System.out.println("====before startActivity====");
+
+
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) !=
+                    PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CALL_PHONE},
+                        MY_PERMISSIONS_REQUEST_CALL_PHONE);
+
+                return;
+            }
+
+            startActivity(callIntent);
+            System.out.println("=====getcallActivity==="+getCallingActivity());
+
+
+        } catch (ActivityNotFoundException e) {
+            Log.e("helloAndroid","Call failed",e);
+        }
+    }
     public void getContacts() {
         contactList = new ArrayList<String>();
+        phonenomber= new ArrayList<String>();
         String phoneNumber = null;
         String email = null;
         Uri CONTENT_URI = ContactsContract.Contacts.CONTENT_URI;
@@ -96,6 +146,7 @@ public class MainActivity extends Activity {
         String EmailCONTACT_ID = ContactsContract.CommonDataKinds.Email.CONTACT_ID;
         String DATA = ContactsContract.CommonDataKinds.Email.DATA;
         StringBuffer output;
+        StringBuffer phone;
         ContentResolver contentResolver = getContentResolver();
         cursor = contentResolver.query(CONTENT_URI, null,null, null, null);
         // Iterate every contact in the phone
@@ -103,6 +154,7 @@ public class MainActivity extends Activity {
             counter = 0;
             while (cursor.moveToNext()) {
                 output = new StringBuffer();
+                phone= new StringBuffer();
                 // Update the progress message
                 updateBarHandler.post(new Runnable() {
                     public void run() {
@@ -119,6 +171,7 @@ public class MainActivity extends Activity {
                     while (phoneCursor.moveToNext()) {
                         phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(NUMBER));
                         output.append("\n Phone number:" + phoneNumber);
+                        phone.append(phoneNumber);
                     }
                     phoneCursor.close();
                     // Read every email id associated with the contact
@@ -131,6 +184,7 @@ public class MainActivity extends Activity {
                 }
                 // Add the contact to the ArrayList
                 contactList.add(output.toString());
+                phonenomber.add(phone.toString());
             }
             // ListView has to be updated using a ui thread
             runOnUiThread(new Runnable() {
